e7d290baeabe994487dfefc10571c1d8168ef85d
natsu08-n <merrysheep18@gmail.com>
2022-04-20 23:35:47 +0900

    カードタイトルのコンポーネント作成

--- react-trello-project/trello-clone/src/components/task/TaskCardTitle.jsx
+++ react-trello-project/trello-clone/src/components/task/TaskCardTitle.jsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const TaskCardTitle = () => {
-	return <div></div>
-}
+	//クリックした時の状態を管理
+	const [isClick, setIsClick] = useState(false);
+	//入力内容の状態を管理
+	const [inputCardTitle, setInputCardTitle] = useState('Today');
+
+	const handleClick = () => {
+		setIsClick(true);
+		//console.log(isClick) isClickはstate変数だからfalseかtrueが入ってくる
+	};
+
+	const handleChange = (e) => {
+		setInputCardTitle(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault(); //Enter押した時(onSubmit時)にページが更新されてしまうのを防ぐ
+		setIsClick(false);
+	};
+
+	return (
+		<div onClick={handleClick}>
+			{isClick ? (
+				<form onSubmit={handleSubmit}>
+					<input type="text" onChange={handleChange} />
+				</form>
+			) : (
+				<h3>{inputCardTitle}</h3>
+			)}
+		</div>
+	);
+};
