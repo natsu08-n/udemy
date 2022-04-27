cbeb53fe20df4e47b7ae2385cc4dea38149ba5f4
natsu08-n <merrysheep18@gmail.com>
2022-04-27 23:57:39 +0900

    入力内容のstate管理

--- react-trello-project/trello-clone/src/components/input/TaskAddInput.jsx
+++ react-trello-project/trello-clone/src/components/input/TaskAddInput.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export const TaskAddInput = () => {
-	return <div></div>
-}
+export const TaskAddInput = ({
+	inputText,
+	setInputText,
+	taskList,
+	setTaskList,
+}) => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	const handleChange = (e) => {
+		setInputText(e.target.value);
+	};
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="add a task"
+					className="taskAddInput"
+					onChange={handleChange}
+				/>
+			</form>
+		</div>
+	);
+};
