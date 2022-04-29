4a81a8724a3d422198feb34c4bb742adf6e36e05
natsu08-n <merrysheep18@gmail.com>
2022-04-29 20:12:25 +0900

    taskコンポーネントの作成

--- react-trello-project/trello-clone/src/components/task/Tasks.jsx
+++ react-trello-project/trello-clone/src/components/task/Tasks.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Task } from './Task';
 
 //{props名}で受け取る
-export const Tasks = ({ inputText, taskList }) => {
+export const Tasks = ({ taskList }) => {
 	return (
 		<div>
 			{taskList.map((task) => (
-				<div>{task.text}</div>
+				<div>
+					<Task task={task} />
+				</div>
 			))}
 		</div>
 	);
