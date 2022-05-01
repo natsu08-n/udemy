fd69be20d7813a5ba9005c685461a80b7a48f465
natsu08-n <merrysheep18@gmail.com>
2022-05-01 23:52:54 +0900

    カードリストエリアの追加処理を実装

--- react-trello-project/trello-clone/src/components/button/AddTaskCardButton.jsx
+++ react-trello-project/trello-clone/src/components/button/AddTaskCardButton.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
-export const AddTaskCardButton = () => {
+export const AddTaskCardButton = ({ taskCardList, setTaskCardList }) => {
+	const taskCardId = uuid();
+	const addTaskCard = () => {
+		setTaskCardList([
+			...taskCardList,
+			{ id: taskCardId, draggableId: `item${taskCardId}` },
+		]);
+	};
 	return (
 		<div className="addTaskCardButtonArea">
-			<button className="addTaskCardButton">+</button>
+			<button className="addTaskCardButton" onClick={addTaskCard}>
+				+
+			</button>
 		</div>
 	);
 };
--- react-trello-project/trello-clone/src/components/task/TaskCards.jsx
+++ react-trello-project/trello-clone/src/components/task/TaskCards.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TaskCard } from './TaskCard';
 import { AddTaskCardButton } from '../button/AddTaskCardButton';
 
 export const TaskCards = () => {
+	//初期段階でカードリストエリアを一つだけ出しておく
+	const [taskCardList, setTaskCardList] = useState([
+		{ id: 0, draggableId: 'item0' },
+	]);
 	return (
 		<div className="taskCardsArea">
-			<TaskCard />
-			<AddTaskCardButton />
+			{taskCardList.map((taskCardListItem) => (
+				<TaskCard key={taskCardListItem.id} />
+			))}
+			<AddTaskCardButton
+				taskCardList={taskCardList}
+				setTaskCardList={setTaskCardList}
+			/>
 		</div>
 	);
 };
