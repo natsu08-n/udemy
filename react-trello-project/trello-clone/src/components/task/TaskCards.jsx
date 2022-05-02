6d6acba34ba361fbaed8528e6d7ff59cf9dfe6f7
natsu08-n <merrysheep18@gmail.com>
2022-05-02 21:44:06 +0900

    タスクカードのドラッグ&ドロップ実装

--- react-trello-project/trello-clone/src/components/task/TaskCards.jsx
+++ react-trello-project/trello-clone/src/components/task/TaskCards.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import { TaskCard } from './TaskCard';
 import { AddTaskCardButton } from '../button/AddTaskCardButton';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 export const TaskCards = () => {
 	//初期段階でカードリストエリアを一つだけ出しておく
 	const [taskCardList, setTaskCardList] = useState([
-		{ id: 0, draggableId: 'item0' },
+		{ id: '0', draggableId: 'item0' },
 	]);
 	return (
-		<div className="taskCardsArea">
-			{taskCardList.map((taskCardListItem) => (
-				<TaskCard key={taskCardListItem.id} />
-			))}
-			<AddTaskCardButton
-				taskCardList={taskCardList}
-				setTaskCardList={setTaskCardList}
-			/>
-		</div>
+		<DragDropContext>
+			<Droppable droppableId="droppable" direction="horizontal">
+				{(provided) => (
+					<div
+						className="taskCardsArea"
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+					>
+						{taskCardList.map((taskCard, index) => (
+							<TaskCard
+								key={taskCard.id}
+								index={index}
+								taskCardList={taskCardList}
+								setTaskCardList={setTaskCardList}
+								taskCard={taskCard}
+							/>
+						))}
+						{provided.placeholder}
+						<AddTaskCardButton
+							taskCardList={taskCardList}
+							setTaskCardList={setTaskCardList}
+						/>
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
 	);
 };
