aefd510c220469bc7c63d9754a56791d240c336c
natsu08-n <merrysheep18@gmail.com>
2022-05-02 22:24:48 +0900

    タスクカードの入れ替え機能実装

--- react-trello-project/trello-clone/src/components/task/TaskCards.jsx
+++ react-trello-project/trello-clone/src/components/task/TaskCards.jsx
@@ -3,13 +3,24 @@ import { TaskCard } from './TaskCard';
 import { AddTaskCardButton } from '../button/AddTaskCardButton';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
+const reOrder = (taskCardList, startIndex, endIndex) => {
+	//タスクを並び変える
+	const remove = taskCardList.splice(startIndex, 1);
+	taskCardList.splice(endIndex, 0, remove[0]);
+};
+
 export const TaskCards = () => {
 	//初期段階でカードリストエリアを一つだけ出しておく
 	const [taskCardList, setTaskCardList] = useState([
 		{ id: '0', draggableId: 'item0' },
 	]);
+	const handleDragEnd = (result) => {
+		reOrder(taskCardList, result.source.index, result.destination.index);
+
+		setTaskCardList(taskCardList);
+	};
 	return (
-		<DragDropContext>
+		<DragDropContext onDragEnd={handleDragEnd}>
 			<Droppable droppableId="droppable" direction="horizontal">
 				{(provided) => (
 					<div
