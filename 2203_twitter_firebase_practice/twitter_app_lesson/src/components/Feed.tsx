f52e8b2bfe2db8ecfa134c6a718a4468698ad348
natsu08-n <merrysheep18@gmail.com>
2022-06-11 13:35:26 +0900

    ログインユーザーアバター画像

--- 2203_twitter_firebase_practice/twitter_app_lesson/src/components/Feed.tsx
+++ 2203_twitter_firebase_practice/twitter_app_lesson/src/components/Feed.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
 
 const Feed = () => {
 	return (
-		<div>Feed
+		<div>
+			Feed
+			<TweetInput />
 			<button onClick={() => auth.signOut()}>logout</button>
 		</div>
-	)
-}
+	);
+};
 
-export default Feed
+export default Feed;
--- 2203_twitter_firebase_practice/twitter_app_lesson/src/features/userSlice.ts
+++ 2203_twitter_firebase_practice/twitter_app_lesson/src/features/userSlice.ts
@@ -2,30 +2,30 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
 
 interface USER {
-  displayName: string;
-  photoUrl: string;
+	displayName: string;
+	photoUrl: string;
 }
 
 export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    //Reduxのstoreで扱いたいステートを定義する、TSが推論で型付けをしてくれる
-    user: { uid: "", photoUrl: "", displayName: "" },
-  },
-  reducers: {
-    //actionのペイロードの属性にReactのコンポーネントからdispatchで呼び出すときに引数としてfirebaseから取得したUserの情報をactionのペイロードに渡してログインで受け取れるようにする
-    login: (state, action) => {
-      state.user = action.payload;
-    },
-    logout: (state) => {
-      //userの情報を初期化
-      state.user = { uid: "", photoUrl: "", displayName: "" };
-    },
-    updateUserProfile: (state, action: PayloadAction<USER>) => {
-      state.user.displayName = action.payload.displayName;
-      state.user.photoUrl = action.payload.photoUrl;
-    }
-  },
+	name: "user",
+	initialState: {
+		//Reduxのstoreで扱いたいステートを定義する、TSが推論で型付けをしてくれる
+		user: { uid: "", photoUrl: "", displayName: "" },
+	},
+	reducers: {
+		//actionのペイロードの属性にReactのコンポーネントからdispatchで呼び出すとき、引数としてfirebaseから取得したUserの情報をactionのペイロードに渡してログインで受け取れるようにする
+		login: (state, action) => {
+			state.user = action.payload;
+		},
+		logout: (state) => {
+			//userの情報を初期化
+			state.user = { uid: "", photoUrl: "", displayName: "" };
+		},
+		updateUserProfile: (state, action: PayloadAction<USER>) => {
+			state.user.displayName = action.payload.displayName;
+			state.user.photoUrl = action.payload.photoUrl;
+		},
+	},
 });
 
 export const { login, logout, updateUserProfile } = userSlice.actions;
