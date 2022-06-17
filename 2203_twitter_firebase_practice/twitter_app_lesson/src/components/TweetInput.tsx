fc4e5a65c7fff1b8122821334a24146aeeecad5c
natsu08-n <merrysheep18@gmail.com>
2022-06-17 11:40:47 +0900

    Tweet投稿フォーム

--- 2203_twitter_firebase_practice/twitter_app_lesson/src/components/TweetInput.tsx
+++ 2203_twitter_firebase_practice/twitter_app_lesson/src/components/TweetInput.tsx
@@ -11,6 +11,7 @@ const TweetInput = () => {
 	const user = useSelector(selectUser);
 	const [tweetImage, setTweetImage] = useState<File | null>(null);
 	const [tweetMsg, setTweetMsg] = useState("");
+
 	const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files![0]) {
 			//!はnonnulltypeアサーション
@@ -18,61 +19,104 @@ const TweetInput = () => {
 			e.target.value = ""; //htmlのファイルダイアログを毎回同じものを指定したときに毎回反応しない仕様、そのため毎回空の文字列で初期化する必要がある
 		}
 	};
-	const sendTweet = (e: React.FormEvent<HTMLFormElement>) {
+
+	const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault(); //送信したときにブラウザが自動リフレッシュされてしまうので、それを防ぐ
 
-		if(tweetImage) {
+		if (tweetImage) {
 			const S =
-			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUCVWXYZ0123456789";
-		const N = 16;
-		const randomeChar = Array.from(crypto.getRandomValues(new Uint32Array(N))) //Crypto.getRandomValues() メソッドは、暗号強度の強い乱数値を取得する
-			.map((n) => S[n % S.length])
-			.join("");
+				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUCVWXYZ0123456789";
+			const N = 16;
+			const randomeChar = Array.from(crypto.getRandomValues(new Uint32Array(N))) //Crypto.getRandomValues() メソッドは、暗号強度の強い乱数値を取得する
+				.map((n) => S[n % S.length])
+				.join("");
 
-		const fileName = randomeChar + "_" + tweetImage.name;
-		const uploadTweetImg = storage.ref(`images/${fileName}`).put(tweetImage);
-		uploadTweetImg.on(
-			//storageの処理に何らかの変更があった場合の後処理を追加できる
-			firebase.storage.TaskEvent.STATE_CHANGED,
-			//引数3つ取れる
-			() => {}, //進捗状況管理、必要ないので空で
-			(err) => { alert(err.message)} ,//エラーハンドリング
-			async () => { await storage.ref("images").child(fileName).getDownloadURL().then(
-				async (url) => {
-					await db.collection("posts").add({
-						avatar: user.photoUrl,
-						image: url,
-						text: tweetMsg,
-						timestamp: firebase.firestore.FieldValue.serverTimestamp(),　//firebaseのサーバータイム
-						username: user.displayName,
-					})
+			const fileName = randomeChar + "_" + tweetImage.name;
+			const uploadTweetImg = storage.ref(`images/${fileName}`).put(tweetImage);
+			uploadTweetImg.on(
+				//storageの処理に何らかの変更があった場合の後処理を追加できる
+				firebase.storage.TaskEvent.STATE_CHANGED,
+				//引数3つ取れる
+				() => {}, //進捗状況管理、必要ないので空で
+				(err) => {
+					alert(err.message);
+				}, //エラーハンドリング
+				async () => {
+					await storage
+						.ref("images")
+						.child(fileName)
+						.getDownloadURL()
+						.then(async (url) => {
+							await db.collection("posts").add({
+								avatar: user.photoUrl,
+								image: url,
+								text: tweetMsg,
+								timestamp: firebase.firestore.FieldValue.serverTimestamp(), //firebaseのサーバータイム
+								username: user.displayName,
+							});
+						});
 				}
-			)}
-			) 
-
+			);
 		} else {
 			db.collection("posts").add({
 				avatar: user.photoUrl,
 				image: "",
 				text: tweetMsg,
-				timestamp: firebase.firestore.FieldValue.serverTimestamp(),　//firebaseのサーバータイム
+				timestamp: firebase.firestore.FieldValue.serverTimestamp(), //firebaseのサーバータイム
 				username: user.displayName,
 			});
 		}
 		//投稿が終わったら初期化
 		setTweetImage(null);
 		setTweetMsg("");
-	}
+	};
 	return (
-		<div>
-			<Avatar
-				className={styles.tweet_avatar}
-				src={user.photoUrl}
-				onClick={async () => {
-					await auth.signOut();
-				}}
-			/>
-		</div>
+		<>
+			<form onSubmit={sendTweet}>
+				<div className={styles.tweet_form}>
+					<Avatar
+						className={styles.tweet_avatar}
+						src={user.photoUrl}
+						onClick={async () => {
+							await auth.signOut();
+						}}
+					/>
+
+					<input
+						type="text"
+						className={styles.tweet_input}
+						placeholder="what's happening?"
+						autoFocus //ウェブページが表示された際に、指定した入力欄にカーソルが当たって自動的にフォーカスされます。
+						value={tweetMsg}
+						onChange={(e) => setTweetMsg(e.target.value)}
+					/>
+
+					<IconButton>
+						<label>
+							<AddAPhotoIcon
+								className={
+									tweetImage ? styles.tweet_addIconLoaded : styles.tweet_addIcon
+								}
+							/>
+							<input
+								type="file"
+								className={styles.tweet_hiddenIcon}
+								onChange={onChangeImageHandler}
+							/>
+						</label>
+					</IconButton>
+				</div>
+				<Button
+					type="submit"
+					disabled={!tweetMsg}
+					className={
+						tweetMsg ? styles.tweet_sendBtn : styles.tweet_sendDisableBtn
+					}
+				>
+					送信
+				</Button>
+			</form>
+		</>
 	);
 };
 
