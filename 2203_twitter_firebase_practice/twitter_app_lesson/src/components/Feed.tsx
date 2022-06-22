5862e6e5fc43fcfab49190afddf1c72bbff56cd3
natsu08-n <merrysheep18@gmail.com>
2022-06-22 10:32:30 +0900

    投稿一覧機能

--- 2203_twitter_firebase_practice/twitter_app_lesson/src/components/Feed.tsx
+++ 2203_twitter_firebase_practice/twitter_app_lesson/src/components/Feed.tsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Feed.module.css";
-import { auth } from "../firebase";
+import { db } from "../firebase";
 import TweetInput from "./TweetInput";
+import Post from "./Post";
 
-const Feed = () => {
+const Feed: React.FC = () => {
+	const [posts, setPosts] = useState([
+		{
+			id: "",
+			avatar: "",
+			image: "",
+			text: "",
+			timestamp: null,
+			username: "",
+		},
+	]);
+
+	//feedコンポーネントがマウントされたとき、useEffectの内容が実行される(firebaseから取ってくる)
+	useEffect(() => {
+		const unSub = db
+			.collection("posts")
+			.orderBy("timestamp", "desc") //最新の内容から降順
+			.onSnapshot((snapshot) =>
+				setPosts(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						avatar: doc.data().avatar,
+						image: doc.data().image,
+						text: doc.data().text,
+						timestamp: doc.data().timestamp,
+						username: doc.data().username,
+					}))
+				)
+			);
+		//アンマウントするときにクリーンアップ関数として実行される
+		return () => {
+			unSub();
+		};
+	}, []);
 	return (
 		<div className={styles.feed}>
-			Feed
 			<TweetInput />
-			<button onClick={() => auth.signOut()}>logout</button>
+			{posts[0]?.id && (
+				<>
+					{posts.map((post) => (
+						<Post
+							key={post.id}
+							postId={post.id}
+							avatar={post.avatar}
+							image={post.image}
+							text={post.text}
+							timestamp={post.timestamp}
+							username={post.username}
+						/>
+					))}
+				</>
+			)}
 		</div>
 	);
 };
